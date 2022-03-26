9763cee1875d5cc11083ea527269e81b758e4854
lolguy416859 <lolguy416859@gmail.com>
2022-03-25 22:28:09 -0500

    Update Texal.java

--- TexalProject/src/main/java/io/github/dadpea/texal/Texal.java
+++ TexalProject/src/main/java/io/github/dadpea/texal/Texal.java
@@ -3,9 +3,10 @@ package io.github.dadpea.texal;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Texal extends JavaPlugin {
+
     @Override
     public void onEnable() {
-        System.out.println("Hello, World!");
+        getLogger().info("Hello, World!");
     }
     @Override
     public void onDisable() {
