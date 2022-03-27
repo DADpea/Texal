f5fef9e157fb99e310880d3b761b31a53ed7fb4f
DADpea <40965707+DADpea@users.noreply.github.com>
2022-03-27 17:48:42 -0600

    help

--- TexalProject/src/main/java/io/github/dadpea/texal/Texal.java
+++ TexalProject/src/main/java/io/github/dadpea/texal/Texal.java
@@ -1,10 +1,13 @@
 package io.github.dadpea.texal;
 
 import io.github.dadpea.texal.events.*;
+import io.github.dadpea.texal.style.ColorConvert;
+import io.github.dadpea.texal.style.GlobalColors;
 import io.github.dadpea.texal.style.Prefix;
 import io.github.dadpea.texal.commands.*;
 import io.github.dadpea.texal.commands.itemManipulation.*;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,13 +15,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
 public final class Texal extends JavaPlugin {
     public static Location spawnPoint;
+    public static Scoreboard spawnBoard;
+    public static String scoreboardTitle = GlobalColors.TEAL + "" + ChatColor.BOLD + ">" + GlobalColors.GREEN + "" + ChatColor.BOLD + "  TEXAL  " + GlobalColors.TEAL + ChatColor.BOLD + "<";
 
     @Override
     public void onEnable() {
         spawnPoint =  new Location(Bukkit.getWorld("world"), 0.5, 2, 0.5);
+        spawnBoard = Bukkit.getScoreboardManager().getNewScoreboard();
+        Objective obj = spawnBoard.registerNewObjective("spawnBoard", "dummy", scoreboardTitle);
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        obj.getScore(" ").setScore(0);
+        obj.getScore(ChatColor.GRAY + "Spawn" + ChatColor.DARK_GRAY + " - " + GlobalColors.WHITE + "Patio").setScore(1);
+        obj.getScore("  ").setScore(2);
+
         registerCommands();
         registerEvents();
     }
--- TexalProject/src/main/java/io/github/dadpea/texal/events/JoinEvent.java
+++ TexalProject/src/main/java/io/github/dadpea/texal/events/JoinEvent.java
@@ -3,10 +3,12 @@ package io.github.dadpea.texal.events;
 import io.github.dadpea.texal.Texal;
 import io.github.dadpea.texal.style.Prefix;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.Scoreboard;
 
 public class JoinEvent implements Listener {
     @EventHandler
@@ -17,5 +19,14 @@ public class JoinEvent implements Listener {
             p1.sendMessage(Prefix.PREFIX_SERVER_INFO + "" + p.getDisplayName() + " joined!");
         }
         p.teleport(Texal.spawnPoint);
+        p.setFlySpeed(0.1f);
+        p.setWalkSpeed(0.1f);
+        p.setExp(0f);
+        p.setLevel(0);
+        p.setHealth(20);
+        p.setFlying(false);
+        p.setGameMode(GameMode.ADVENTURE);
+        p.setScoreboard(Texal.spawnBoard);
+        p.getInventory().clear();
     }
 }
