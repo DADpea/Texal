0678bf5de5da2edd8a8e7964e9f320ca1c6769f4
lolguy416859 <lolguy416859@gmail.com>
2022-03-27 00:49:16 -0500

    Added translateColorCode()

--- TexalProject/src/main/java/io/github/dadpea/texal/ColorConvert.java
+++ TexalProject/src/main/java/io/github/dadpea/texal/ColorConvert.java
@@ -1,18 +1,34 @@
 package io.github.dadpea.texal;
 
 import org.bukkit.ChatColor;
+import static org.bukkit.ChatColor.COLOR_CHAR;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ColorConvert {
     public static String fromHex(String s) {
         if (s.length()!=7) {
             throw new IllegalArgumentException("Hex must be seven chars, beginning with a #");
         }
-        return ChatColor.COLOR_CHAR + "x"
-             + ChatColor.COLOR_CHAR + "" + s.charAt(1)
-             + ChatColor.COLOR_CHAR + "" + s.charAt(2)
-             + ChatColor.COLOR_CHAR + "" + s.charAt(3)
-             + ChatColor.COLOR_CHAR + "" + s.charAt(4)
-             + ChatColor.COLOR_CHAR + "" + s.charAt(5)
-             + ChatColor.COLOR_CHAR + "" + s.charAt(6);
+        return COLOR_CHAR + "x"
+             + COLOR_CHAR + "" + s.charAt(1)
+             + COLOR_CHAR + "" + s.charAt(2)
+             + COLOR_CHAR + "" + s.charAt(3)
+             + COLOR_CHAR + "" + s.charAt(4)
+             + COLOR_CHAR + "" + s.charAt(5)
+             + COLOR_CHAR + "" + s.charAt(6);
+    }
+
+    public static String translateColorCodes(String s) {
+        return s.replaceAll("#([0-9A-Fa-f]){6}",
+COLOR_CHAR + "x"
+        + COLOR_CHAR + "$1"
+        + COLOR_CHAR + "$2"
+        + COLOR_CHAR + "$3"
+        + COLOR_CHAR + "$4"
+        + COLOR_CHAR + "$5"
+        + COLOR_CHAR + "$6"
+        ).replaceAll("&([A-Fa-f0-9])", COLOR_CHAR + "$1");
     }
 }
